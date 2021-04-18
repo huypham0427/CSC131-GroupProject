adf51c6830ac378dd95bfeac812f8f5583f25af8
huypham0427 <59068418+huypham0427@users.noreply.github.com>
2021-04-18 13:34:04 -0700

    Add Movie and Award class

---
+++ CSC131Project-master/CSC131-main/simple-rest-apis-master/src/main/java/com/example/simplerestapis/Movies.java
@@ -0,0 +1,57 @@
+package com.example.simplerestapis;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Movies {
+
+    private String title;
+    private String year;
+    private String ceremony;
+    private String category;
+    private ArrayList<Award> awards;
+
+
+    public Movies(String year, String title, String ceremony)
+    {
+        this.title = title;
+        this.year = year;
+        this.ceremony = ceremony;
+        this.awards = new ArrayList<>();
+
+    }
+
+
+    public void addAward(Award award){
+        awards.add(award);
+    }
+
+//    public void getCategory(Award category){
+//        awards.add(category);
+//    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getYear()
+    {
+        return year;
+    }
+
+    public String getCeremony()
+    {
+        return ceremony;
+    }
+
+    public List<Award> getAwards()
+    {
+        return awards;
+    }
+
+    @Override
+    public String toString(){
+        return "Category: " + category;
+    }
+}
